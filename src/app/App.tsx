a846423baa0af7ade9e3a807c120752abe22491d
Andrew Kostromin <akostromin@sitronics.com>
2025-07-02 17:06:07 +0300

    initial

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export const App = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.content}>Hello world</div>
+    </div>
+  );
+};
